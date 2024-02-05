7bfae1fa9c63a84cf929811e6b7c9f539640be5c
Nile <nilecameron1@gmail.com>
2024-02-05 18:04:03 +0000

    End of chapter 5, passing all tests seems to be working!!

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -3,5 +3,12 @@ from rango.models import Category, Page
 
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+    
+admin.site.register(Page, PageAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'likes')
+    
+admin.site.register(Category, CategoryAdmin)
